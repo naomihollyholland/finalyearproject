98552b0b228a273c7af528701c4e9f5e03e8b1b4
josephholland07 <joeholland2107@gmail.com>
2026-02-13 11:09:25 +0000

    shows ID of each node
    shows the node id of each node, so the user can specify which one to delete in future update

--- finalyearproject/src/routes/binary_tree/+page.svelte
+++ finalyearproject/src/routes/binary_tree/+page.svelte
@@ -1,26 +1,126 @@
-<script>
+<script lang="ts">
+    import { get } from "svelte/store";
     import TreeNode from "./TreeNode.svelte";
 
-    let i= $state([150,150]);
+    interface Node {
+        id: number;
+        val: number;
+        x: number;
+        y: number;
+        parentid: number | null;
+        lchildid: number | null;
+        rchildid: number | null;
+    }
+    let i = $state([500, 150]);
+
+    let nodeinputvalue: number = $state(-1);
 
     let allnodes = $state([
-         {id:0, val:1, x: i[0], y:i[1], parentid: null},
-         {id:1, val:2, x: 250, y:250, parentid: 0},
-         {id:2, val:3, x: 350, y:350, parentid: 1}
-     ]);
+        {
+            id: 0,
+            val: 1,
+            x: i[0],
+            y: i[1],
+            parentid: null,
+            lchildid: null,
+            rchildid: null
+        },
 
-    for(let node of allnodes){
-            {allnodes[node.id].x = (allnodes.find(n => n.id === node.parentid)?.x || i[0] - 100) + 100}
-            {allnodes[node.id].y = (allnodes.find(n => n.id === node.parentid)?.y || i[1] - 100) + 100}
-        
+    ]);
+
+    for (let node of allnodes) {
+        if (node.parentid != null && node != null) {
+            let parentnode = allnodes[node.parentid];
+            if (node.val < parentnode.val) {
+                node.x = parentnode.x - 100;
+                node.y = parentnode.y + 100;
+            } else {
+                node.x = parentnode.x + 100;
+                node.y = parentnode.y + 100;
+            }
+        }
+    }
+
+    function findid(node: Node) {
+        return node.id;
+    }
+
+
+    function getroot() {
+        let root = allnodes.find((node) => node.parentid === null) || -1;
+        return root;
+    }
+
+    function comparenodes(node1: Node, node2: Node) {
+        if (node1.val < node2.val) return -1;
+        if (node1.val > node2.val) return 1;
+        return 0;
+    }
+
+    function placenode(node1: Node, node2: Node) {
+        if (comparenodes(node1, node2) <= 0) {
+                if (node2.lchildid == null) {
+                    node2.lchildid = node1.id;
+                    node1.parentid = node2.id;
+                    node1.x = node2.x - 150;
+                    node1.y = node2.y + 100;
+                    //console.log(node1, node2);
+                } else{
+                    console.log(allnodes.find((node) => node.id === node2.lchildid) as Node);
+                    placenode(node1, allnodes.find((node) => node.id === node2.lchildid) as Node);
+                }
+            }
+                else if (comparenodes(node1, node2) > 0) {
+                    if (node2.rchildid == null) {
+                        node2.rchildid = node1.id;
+                        node1.parentid = node2.id;
+                        node1.x = node2.x + 150;
+                        node1.y = node2.y + 100;
+                        //console.log(node1, node2);
+                    } else {
+                        placenode(node1, allnodes.find((node) => node.id === node2.rchildid) as Node);
+                    }
+                }
+                else {
+
+                    return;
+                }
+                }
+                
+    
+    
+
+    function push() {
+        if (nodeinputvalue == -1) return;
+        let node = {
+            id: allnodes.length,
+            val: nodeinputvalue,
+            x: 500,
+            y: 500,
+            parentid: null,
+            lchildid: null,
+            rchildid: null,
+        };
+
+        placenode(node, getroot() as Node);
+        console.log($state.snapshot(node));
+
+        console.log($state.snapshot(allnodes));
+        allnodes.push(node);
+
+
+        //get the node where there is no parent
+
+        //start comparing to left or right child until there is no child, then add the node there
+
+        nodeinputvalue = -1;
     }
 </script>
 
 <h1>Binary tree page!</h1>
-
-
+<input bind:value={nodeinputvalue} placeholder=-1 type="number" />
+<button onclick={push}>Add node</button>
 
 {#each allnodes as node}
-    {node.id} <br>
-        <TreeNode i={node.val}  x={node.x} y={node.y}/>  
+    <TreeNode id = {node.id} value={node.val} x={node.x} y={node.y} />
 {/each}
--- finalyearproject/src/routes/binary_tree/TreeNode.svelte
+++ finalyearproject/src/routes/binary_tree/TreeNode.svelte
@@ -1,12 +1,20 @@
 <script>
-    let {i,x,y} = $props();
+    let {id, value ,x,y} = $props();
 </script>
 
 <div class="circle" style="top: {y}px; left: {x}px">
-    {i}
+    <div class="id">
+    ID: {id}
+    </div>
+    <div class="value">
+    {value}
+    </div>
 </div>
 
+
 <style>
+
+    
     .circle{
         width: 100px;
         height: 100px;
@@ -18,5 +26,25 @@
         align-items: center;
         position:absolute;
         transition: 1s;
+        flex-direction: column;
     }
+
+    .id{
+        height: 30%;
+
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+
+     .value{
+
+        height: 50%;
+        flex-direction: row;
+        flex-wrap: wrap;
+
+    }
+
+
+
 </style>
